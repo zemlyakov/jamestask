b1d9aeecf69e3c3fa3aeeb066876fd454c5b662b
zemlyakov <zemliakov@protonmail.com>
2022-10-13 14:15:02 +0400

    Add Chart component

---
+++ src/components/Chart.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
+const options = {
+  chart: {
+    type: "spline",
+  },
+  title: {
+    text: "My chart",
+  },
+  series: [
+    {
+      data: [1, 2, 1, 4, 3, 6],
+    },
+  ],
+};
+
+const Chart = () => {
+  return (
+    <div>
+      <HighchartsReact highcharts={Highcharts} options={options} />
+    </div>
+  );
+};
+
+export default Chart;
